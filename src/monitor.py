4f971622290d18d3acc2428f1e6d6fb1a710e5a5
Nicolas Jakob <jakobnicolas@gmail.com>
2021-04-12 21:53:02 +0200

    script: add logging

--- src/monitor.py
+++ src/monitor.py
@@ -1,9 +1,12 @@
 import argparse
 import json
+import logging
 
 parser = argparse.ArgumentParser('Monitor your Win8/10 FileHistory instances from a server')
 parser.add_argument(dest='config', help='the configuration file path in xml')
 args = parser.parse_args()
+
+logging.info("Parameters = " + str(args))
 
 with open(args.config) as f:
   config = json.load(f)
