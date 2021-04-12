436b17d9bc9ccbd0b08121130775e622e6f3542a
Nicolas Jakob <jakobnicolas@gmail.com>
2021-04-12 21:36:47 +0200

    script: add input parameter parsing

--- src/monitor.py
+++ src/monitor.py
@@ -1,6 +1,9 @@
+import argparse
 import json
 
-path = '/config/config.json'
+parser = argparse.ArgumentParser('Monitor your Win8/10 FileHistory instances from a server')
+parser.add_argument(dest='config', help='the configuration file path in xml')
+args = parser.parse_args()
 
-with open(path) as f:
+with open(args.config) as f:
   config = json.load(f)
