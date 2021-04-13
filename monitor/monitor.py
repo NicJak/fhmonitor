6548cf66be8513c066736db9739fe145370b28ab
Nicolas Jakob <jakobnicolas@gmail.com>
2021-04-13 12:34:57 +0200

    script: search for files to monitor automatically

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,8 +1,8 @@
 import argparse
 import json
 import logging
+import os
 import threading
-import time
 
 import requests
 from inotify_simple import INotify, flags
@@ -10,19 +10,39 @@ from inotify_simple import INotify, flags
 
 def wait_for_change(path):
     inotify = INotify()
-    try:
-        inotify.add_watch(path, flags.CLOSE_WRITE)
-        inotify.read()
-        logging.info(path + " changed")
-    except FileNotFoundError:
-        logging.warning("File does not exist: " + path)
-        time.sleep(60)
+    inotify.add_watch(path, flags.CLOSE_WRITE)
+    inotify.read()
+    logging.info(path + " changed")
 
 
 def call_on_change(path, url):
     while True:
         wait_for_change(path)
         requests.get(url)
+
+
+def get_subdirectories(path):
+    subdirectories = []
+    for directory in next(os.walk(path))[1]:
+        subdirectories.append(os.path.join(path, directory))
+    return subdirectories
+
+
+def get_configuration_directory(path):
+    for directory in next(os.walk(path))[1]:
+        if directory == 'Configuration':
+            return os.path.join(path, directory)
+    return None
+
+
+def get_backup_config_files(path):
+    config_files = []
+    for backup_directory in get_subdirectories(path):
+        config_directory = get_configuration_directory(backup_directory)
+        if config_directory is not None:
+            for config_file in next(os.walk(config_directory))[2]:
+                config_files.append(os.path.join(config_directory, config_file))
+    return config_files
 
 
 parser = argparse.ArgumentParser('Monitor your Win8/10 FileHistory instances from a server')
@@ -36,9 +56,18 @@ with open(args.config) as f:
 
 threads = []
 for backup in config:
-    thread = threading.Thread(target=call_on_change, args=(backup['path'], backup['url']))
-    threads.append(thread)
-    thread.start()
+    base = backup['path']
+    if not os.path.exists(base):
+        logging.warning(base + " does not exist")
+        continue
+    backup_directory = get_backup_config_files(base)
+    if len(backup_directory) == 0:
+        logging.warning("No backup found in " + base)
+    for file in backup_directory:
+        logging.info("monitoring " + file)
+        thread = threading.Thread(target=call_on_change, args=(file, backup['url']))
+        threads.append(thread)
+        thread.start()
 
 for thread in threads:
     thread.join()
