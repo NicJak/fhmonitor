65e190986689fd2a7199bb710dbfa78bb0e22b1d
Nicolas Jakob <jakobnicolas@gmail.com>
2021-04-13 10:54:27 +0200

    script: monitor file and call url on change

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,6 +1,23 @@
 import argparse
 import json
 import logging
+import threading
+import requests
+from inotify_simple import INotify, flags
+
+
+def wait_for_change(path):
+    inotify = INotify()
+    inotify.add_watch(path, flags.CLOSE_WRITE)
+    inotify.read()
+    logging.info(path + " changed")
+
+
+def call_on_change(path, url):
+    while True:
+        wait_for_change(path)
+        requests.get(url)
+
 
 parser = argparse.ArgumentParser('Monitor your Win8/10 FileHistory instances from a server')
 parser.add_argument(dest='config', help='the configuration file path in xml')
@@ -10,3 +27,12 @@ logging.info("Parameters = " + str(args))
 
 with open(args.config) as f:
     config = json.load(f)
+
+threads = []
+for backup in config:
+    thread = threading.Thread(target=call_on_change, args=(backup['path'], backup['url']))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
